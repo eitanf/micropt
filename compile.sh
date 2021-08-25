98b0eb82ef90d6a05e13b35e0d286857e0e4eed0
Eitan Frachtenberg <eitan@frachtenberg.org>
2021-08-25 14:54:50 -0700

    Fix typo in compile script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-COMPILE="g++-5 -Ofast -DNEDBUG -std=c++17 -march=native   -Wall -Wextra -pedantic"
+COMPILE="g++ -Ofast -DNDEBUG -std=c++17 -march=native  -Wall -Wextra -pedantic"
 for fn in *.cc
 do
     $COMPILE -o ${fn%%.*} driver.c $fn &
